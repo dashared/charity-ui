a132f698961c4a36cbdecc7d281f63a05d23d79a
dashared <rednikina.com@yandex.ru>
2021-04-05 20:24:26 +0300

    push notifications
    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { FC, Suspense } from "react";
 import Provider, { useTranslation } from "@providers";
+import { requestFirebaseNotificationPermission } from "@providers/firebase";
 
 import Layout from "./Layout";
 
 const App: FC = () => {
   const { t } = useTranslation();
   const loadingMessage = <div>{t("loading")} </div>;
+
+  requestFirebaseNotificationPermission()
+    .then((firebaseToken) => {
+      // eslint-disable-next-line no-console
+      console.log(firebaseToken);
+    })
+    .catch((err) => {
+      return err;
+    });
 
   return (
     <Suspense fallback={loadingMessage}>
